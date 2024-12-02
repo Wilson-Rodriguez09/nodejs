3b02eb878f7d43575c4fa7bd45ba38707f117e51
wilson-rodriguez09 <samboniwilson09@gamil.com>
2024-12-02 11:38:57 -0500

    jeison web token, validacion de tokens y validacion de usuario

--- src/routers/usuarioRouter.js
+++ src/routers/usuarioRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import {actualiazarUsuario, buscarUsuario, eliminarUsuario, ListarUsuario, registrarUsuario} from "../controllers/usuarioController.js";
+import {validarToken} from '../controllers/autenticacionUsuario.js'
 
 const ruta = Router();
 
-ruta.get("/usuarios",ListarUsuario);
+ruta.get("/usuarios",validarToken,ListarUsuario);
 ruta.get("/usuarios/:id_usuario",buscarUsuario)
 ruta.post("/usuarios",registrarUsuario);
 ruta.put("/usuarios/:id_usuario",actualiazarUsuario);
